7e9a43b971aa1f936ecbac3f0243a838e5263a6b
augusu <kou061488+1@gmail.com>
2026-03-05 01:53:34 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import supabase from './lib/supabase';
+import { supabase } from './lib/supabase'
 import AuthScreen from './AuthScreen';
 import Editor from './Editor';
 import './index.css';
